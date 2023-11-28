d91c60ce1518f6b4d885eab1fec0659922c20056
cirrahn <9784519+cirrahn@users.noreply.github.com>
2023-11-28 21:25:45 +0000

    chore: clean const structure

--- Main.js
+++ Main.js
@@ -4,33 +4,32 @@ import express from "express";
 import opn from "opn";
 
 class Const {
+	static ALL_SCOPES = [
+		"ugc-image-upload",
+		"user-read-recently-played",
+		"user-top-read",
+		"user-read-playback-position",
+		"user-read-playback-state",
+		"user-modify-playback-state",
+		"user-read-currently-playing",
+		"app-remote-control",
+		"streaming",
+		"playlist-modify-public",
+		"playlist-modify-private",
+		"playlist-read-private",
+		"playlist-read-collaborative",
+		"user-follow-modify",
+		"user-follow-read",
+		"user-library-modify",
+		"user-library-read",
+		"user-read-email",
+		"user-read-private",
+	];
+	static PORT_CALLBACK = 6660;
+	static HOST_CALLBACK = "http://localhost";
+	static ENDPOINT_CALLBACK = "/api/provide-token"
+	static PLAYLIST_ADD_TRACK_LIMIT = 100;
 }
-
-Const.ALL_SCOPES = [
-	"ugc-image-upload",
-	"user-read-recently-played",
-	"user-top-read",
-	"user-read-playback-position",
-	"user-read-playback-state",
-	"user-modify-playback-state",
-	"user-read-currently-playing",
-	"app-remote-control",
-	"streaming",
-	"playlist-modify-public",
-	"playlist-modify-private",
-	"playlist-read-private",
-	"playlist-read-collaborative",
-	"user-follow-modify",
-	"user-follow-read",
-	"user-library-modify",
-	"user-library-read",
-	"user-read-email",
-	"user-read-private",
-];
-Const.PORT_CALLBACK = 6660;
-Const.HOST_CALLBACK = "http://localhost";
-Const.ENDPOINT_CALLBACK = "/api/provide-token"
-Const.PLAYLIST_ADD_TRACK_LIMIT = 100;
 
 class Util {
 	static getJson (path) {
